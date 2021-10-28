60adaadddc563163eb84fda23f86423c3c9c666d
Taha Shanouha <taha.shanouha@asteya.world>
2021-10-28 13:30:13 +0300

    form fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <p>Hello World</p>;
+  const [values, setValues] = useState<Record<string, any>>({});
+
+  const submit = () => {
+    console.log('submitted', values);
+  };
+
+  const onFieldChange = (name: string, value: any, touch = false) => {
+    //let errorsTemp = Object.assign({}, errors);
+    let valuesTemp = Object.assign({}, values);
+    valuesTemp[name] = value;
+    //let touchedTemp = Object.assign({}, touched);
+    setValues(valuesTemp);
+  };
+
+  return (
+    <div className={'mainContainer'}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submit();
+        }}
+      >
+        <div className={'question'}>
+          <label>
+            Question One
+            <input
+              name="questionOne"
+              type="text"
+              value={values['questionOne']}
+              onChange={(v) => onFieldChange('questionOne', v.target.value)}
+            />
+          </label>
+        </div>
+        <div className={'question'}>
+          <label>
+            Question Two
+            <input
+              name="questionTwo"
+              type="text"
+              value={values['questionTwo']}
+              onChange={(v) => onFieldChange('questionTwo', v.target.value)}
+            />
+          </label>
+        </div>
+        <div className={'question'}>
+          <label>
+            Question Three
+            <input
+              name="questionThree"
+              type="text"
+              value={values['questionThree']}
+              onChange={(v) => onFieldChange('questionThree', v.target.value)}
+            />
+          </label>
+        </div>
+        <div className={'question'}>
+          <label>
+            Question Four
+            <input
+              name="questionFour"
+              type="text"
+              value={values['questionFour']}
+              onChange={(v) => onFieldChange('questionFour', v.target.value)}
+            />
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 export default App;
